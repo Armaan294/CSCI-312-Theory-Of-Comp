bae92d819645d5d071c48cfefda097a6ace3ae54
Armaan294 <armaanb434@gmail.com>
2024-04-17 18:05:26 -0400

    Added all 3 different types of cactus

--- dino.js
+++ dino.js
@@ -12,14 +12,14 @@ let dinoY = boardHeight - dinoHeight;
 let dinoImg;
 
 let dino = {
-    x: dinoX,
-    y: dinoY,
+    x : dinoX,
+    y : dinoY,
     width : dinoWidth,
     height : dinoHeight
 }
 
 //Cactus implementation
-let catcusArray = [];
+let cactusArray = [];
 
 let cactus1Width = 34;
 let cactus2Width = 69;
@@ -30,6 +30,9 @@ let cactusX = 700;
 let cactusY = boardHeight - cactusHeight;
 
 let cactus1Img;
+let cactus2Img;
+let cactus3Img;
+
 
 
 window.onload = function() {
@@ -45,11 +48,17 @@ window.onload = function() {
     dinoImg = new Image();
     dinoImg.src = "./img/dino.png";
     dinoImg.onload = function(){
-    context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+        context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
     }
 
     cactus1Img = new Image();
     cactus1Img.src = "./img/cactus1.png";
+
+    cactus2Img = new Image();
+    cactus2Img.src = "./img/cactus2.png";
+
+    cactus3Img = new Image();
+    cactus3Img.src = "./img/cactus3.png";
 
 
 
@@ -59,8 +68,15 @@ window.onload = function() {
 
 function update() {
     requestAnimationFrame(update);
-
+    
+    //dinosaur
     context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+
+    //cactus
+    for (let i = 0; i < cactusArray.length; i++){
+        let cactus = cactusArray[i];
+        context.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height);
+    }
 
 }
 
@@ -68,13 +84,30 @@ function placeCactus() {
 
     //place the cactus down
     let cactus = {
-        img : null
+        img : null,
         x : cactusX,
-        y: cactusY,
+        y : cactusY,
         width : null,
         height: cactusHeight
     }
 
-    letplaceC
+    let placeCactusChance = Math.random();
+
+    if (placeCactusChance > .90) { //10% you get cactus3
+        cactus.img = cactus3Img;
+        cactus.width = cactus3Width;
+        cactusArray.push(cactus);
+    }
+    else if (placeCactusChance > .70) { //30% you get cactus2
+        cactus.img = cactus2Img;
+        cactus.width = cactus2Width;
+        cactusArray.push(cactus);
+    }
+    else if (placeCactusChance > .50) { //50% you get cactus1
+        cactus.img = cactus1Img;
+        cactus.width = cactus1Width;
+        cactusArray.push(cactus);
+    }
 
 }
+
